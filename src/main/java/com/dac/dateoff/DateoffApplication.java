5e47231e6af766d54a6eaa7ddd7fefec46696a85
tuan.phanvan <tuan.phanvan179@gmail.com>
2017-06-09 14:38:48 +0700

    init source

---
+++ src/main/java/com/dac/dateoff/DateoffApplication.java
@@ -0,0 +1,12 @@
+package com.dac.dateoff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DateoffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DateoffApplication.class, args);
+	}
+}
